82a83f5c74557babef644b634f8e1cd7a727e854
Marko <>
2024-07-12 18:16:06 +0200

    improved script to check for files

--- cicd/script.py
+++ cicd/script.py
@@ -8,9 +8,28 @@ SECRET_USER_NAME = os.getenv('SECRET_USER_NAME')
 
 upload_folder = "test"
 
+uploadable_file_names = ["alemannencup.html",
+                         "datenschutz.html",
+                          "FAQ.html",
+                          "impressum.html",
+                          "index.html",
+                          "kontakt.html",
+                          "teams.html",
+                          "training.html",
+                          "verein.html",
+                          # folders
+                          "files",
+                          "images",
+                          "javascript",
+                          "styles",
+                          "startpage.mp4"]
+
+
 def upload_file(ftp, file_path):
   with open(file_path, "rb") as file:
+    print(f"Uploading {file_path} to the FTP server.")
     ftp.storbinary(f"STOR {file_path}", file)
+    
 
 def delete_file(ftp, file_path):
     try:
@@ -18,6 +37,14 @@ def delete_file(ftp, file_path):
       print(f"Deleted {file_path} from the FTP server.")
     except Exception as e:
       print(f"Could not delete {file_path} from the FTP server: {e}")
+      
+      
+def can_upload_file(file_name):
+  for uploadable_file_name in uploadable_file_names:
+    if uploadable_file_name in file_name:
+      return True
+  return False
+  
 
 def main_script(modified_files, deleted_files):
   try:
@@ -40,20 +67,13 @@ def main_script(modified_files, deleted_files):
       print("Specified Uploading folder", upload_folder)
       raise Exception("Folder does not exist")
 
-    # delete everything in the folder
-    print("Deleting everything in the folder")
-    files_in_folder = ftp.nlst(upload_folder)
-    
-    # delete all files and folders in the folder
-    for file in files_in_folder:
-      ftp.delete(file)
-    
-    print("Attempt to upload modified files: ", modified_files)
 
+    print("Attempt to upload modified files: ", modified_files)
     # Upload modified and added files
     for file in modified_files.split():
       if os.path.isfile(file):
-        upload_file(ftp, file)
+        if can_upload_file(file):
+          upload_file(ftp, file)
     
     # Delete removed files
     print("Attempt to delete files: ", deleted_files)
