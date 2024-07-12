40d50fa7f1a197211eb6d16b73bc5a8c26789839
Marko <>
2024-07-12 17:42:15 +0200

    connect using ftp and print files

--- cicd/script.py
+++ cicd/script.py
@@ -1,4 +1,24 @@
 import os
+from ftplib import FTP
 
-secret_one = os.getenv('SECRET_HOST_NAME')
-print(secret_one)
+SECRET_HOST_NAME = os.getenv('SECRET_HOST_NAME')
+SECRET_PASSWORD = os.getenv('SECRET_PASSWORD')
+SECRET_USER_NAME = os.getenv('SECRET_USER_NAME')
+
+try:
+    # create FTP server
+    ftp = FTP(SECRET_HOST_NAME)
+
+    # login to the server
+    ftp.login(user=SECRET_USER_NAME, passwd=SECRET_PASSWORD)
+
+    # list the files in the current directory
+    files = ftp.nlst()
+    print(files)
+
+    # close the connection
+    ftp.quit()
+except Exception as e:
+  print(e)
+  print('Error: Unable to connect to the FTP server')
+
