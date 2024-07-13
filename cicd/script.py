9f37366c819b274067115aa008002f8a256dc09f
Marko <>
2024-07-13 09:10:27 +0200

    change in ymal and script to support #force

--- cicd/script.py
+++ cicd/script.py
@@ -8,6 +8,8 @@ SECRET_PASSWORD = os.getenv('SECRET_PASSWORD')
 SECRET_USER_NAME = os.getenv('SECRET_USER_NAME')
 
 upload_folder = "test"
+number_deleted_files = 0
+number_inserted_files = 0
 
 uploadable_file_names = ["alemannencup.html",
                          "datenschutz.html",
@@ -70,7 +72,8 @@ def delete_folder_contents(ftp, folder_path):
     
     if is_ftp_directory(ftp, folder_path) == False:
       ftp.delete(folder_path)
-      print(f'Deleted file: {folder_path}')
+      number_deleted_files += 1
+      # print(f'Deleted file: {folder_path}')
     else:
       for file in ftp.nlst(folder_path):
         current_path = ftp.pwd()
@@ -78,7 +81,7 @@ def delete_folder_contents(ftp, folder_path):
         delete_folder_contents(ftp, file)
         ftp.cwd(current_path)
       ftp.rmd(folder_path)
-      print(f'Deleted folder: {folder_path}')
+      # print(f'Deleted folder: {folder_path}')
   except Exception as e:
     print(f'Failed to delete {folder_path}, Error: {e}')
 
@@ -113,9 +116,10 @@ def upload_files_recursivly(ftp, local_last_directory, local_full_path, remote_f
     full_local_path = local_full_path + "/" + files
     if can_upload_file(full_local_path):
       files_path = remote_folder_path + "/" + files
-      print(f'Uploading file: {files_path}')
+      # print(f'Uploading file: {files_path}')
       with open(files_path, 'rb') as file:
         ftp.storbinary(f'STOR {files}', file)
+        number_inserted_files += 1
         
   # Recursively upload files in the subdirectories
   for directory in first_level_directories:
@@ -130,16 +134,63 @@ def upload_files_recursivly(ftp, local_last_directory, local_full_path, remote_f
     # print("full_local_path: ", full_local_path)
     # print("remote_folder_path_cur: ", remote_folder_path_cur)
     if can_upload_file(full_local_path):
-      print()
       upload_files_recursivly(ftp, directory, full_local_path, remote_folder_path_cur)
       ftp.cwd("..")
 
   
 
   
+def full_reinstallation(ftp, upload_folder):
+  print("###############################################################")
+  print("Running full reinstallation")
+  # list the files in the current directory
+  root_files = ftp.nlst()
   
+  if upload_folder not in root_files:
+    # throw error if the folder does not exist
+    print("Folder does not exist")
+    # print("Existing Folders: ", root_files)
+    print("Specified Uploading folder", upload_folder)
+    print("Creating test folder")
+    ftp.mkd(upload_folder)
+  else:
+    print("###############################################################")
+    print("Deleting files from the FTP server")
+    # Recursively delete files in the upload folder
+    delete_folder_contents(ftp, upload_folder)
+    print("Deleted all files from the FTP server")
+    print("Number of deleted files: ", number_deleted_files)
+  
+  print()
+  
+  # Upload all files from the Git repository
+  print("###############################################################")
+  print("Uploading files to the FTP server")
+  repo_root_path = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).decode('utf-8').strip()
+  ftp.mkd(upload_folder)
+  upload_files_recursivly(ftp, upload_folder, upload_folder, repo_root_path)
+  print("Uploaded all files to the FTP server")
+  print("Number of inserted files: ", number_inserted_files)
+  
+  
+  
+def exchange_modified_data(ftp, upload_folder, changed_files, deleted_files):
+  print("###############################################################")
+  print("Running exchange modified data")
+  wasdir = False
 
 def main_script():
+  if len(sys.argv) != 4:
+    print("Usage: python script.py <changed_files> <deleted_files> <contains_force>")
+    return
+  changed_files = sys.argv[1]
+  deleted_files = sys.argv[2]
+  contains_force = sys.argv[3].lower() == 'true'  # Convert 'true'/'false' string to boolean
+  
+  print("Changed files:", changed_files)
+  print("Deleted files:", deleted_files)
+  print("Contains force flag:", contains_force)
+
   try:
     # create FTP server
     print("Creating FTP server")
@@ -150,43 +201,33 @@ def main_script():
     ftp.login(user=SECRET_USER_NAME, passwd=SECRET_PASSWORD)
     print("Login successful")
     
-    # list the files in the current directory
-    root_files = ftp.nlst()
-    
-    if upload_folder not in root_files:
-      # throw error if the folder does not exist
-      print("Folder does not exist")
-      # print("Existing Folders: ", root_files)
-      print("Specified Uploading folder", upload_folder)
-      print("Creating test folder")
-      ftp.mkd(upload_folder)
+    if contains_force:
+      full_reinstallation(ftp, upload_folder)
     else:
-      print("Deleting files from the FTP server")
-      # Recursively delete files in the upload folder
-      delete_folder_contents(ftp, upload_folder)
+      exchange_modified_data(ftp, upload_folder, changed_files, deleted_files)
     
-    print()
     
-    # Upload all files from the Git repository
-    print("Uploading files to the FTP server")
-    repo_root_path = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).decode('utf-8').strip()
-    ftp.mkd(upload_folder)
-    upload_files_recursivly(ftp, upload_folder, upload_folder, repo_root_path)
+    
+    
     
 
 
 
     # close the connection
+    print("###############################################################")
     print("Closing the FTP server connection")
     ftp.quit()
   except Exception as e:
     print(e)
     print('Error: Unable to connect to the FTP server')
     
-print("Script executed")
+  print("###############################################################")
+  print("Script finished")
+    
     
     
   
+
   
 if __name__ == "__main__":
   main_script()
