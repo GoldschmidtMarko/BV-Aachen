84cfbb8877463a83a72a41d10ee0bfe1a175dff4
Marko <ma.goldschmidt@web.de>
2026-02-14 01:03:50 +0100

    feat: fixed links and added new files for alemannencup #upload

--- php/email.php
+++ php/email.php
@@ -23,7 +23,7 @@ $message = <<<HTML
     <p>Hallo {$user['vorname']} {$user['nachname']},</p>
 
     <p>Vielen Dank für deine Anmeldung, diese wird an die Turnierleitung übermittelt und spätestens <strong>innerhalb einer Woche</strong> bearbeitet.</p>
-    <p>Die Meldung gilt erst als bestätigt, wenn sie hier (<a href="https://dbv.turnier.de/sport/events.aspx?id=8e25686d-193f-4971-a464-fd9d6a9af5cc">Link</a>) veröffentlicht wurde und nicht als "Nachrücker" gekennzeichnet ist. Sollte deine Meldung nicht innerhalb einer Woche dort zu finden sein, melde dich bitte unter:</p>
+    <p>Die Meldung gilt erst als bestätigt, wenn sie hier (<a href="https://dbv.turnier.de/sport/events.aspx?id=1eb702e0-4333-44f8-bbeb-fe5de2e91269">Link</a>) veröffentlicht wurde und nicht als "Nachrücker" gekennzeichnet ist. Sollte deine Meldung nicht innerhalb einer Woche dort zu finden sein, melde dich bitte unter:</p>
     <p><a href="mailto:anmeldung@alemannen-cup.de">anmeldung@alemannen-cup.de</a></p>
 
     <p>Hier sind die Details:</p>
@@ -71,7 +71,7 @@ $message = <<<HTML
     <p>Hello {$user['vorname']} {$user['nachname']},</p>
 
     <p>Thank you for your registration, it will be forwarded to the tournament management and processed <strong>within a week</strong> at the latest.</p>
-    <p>The registration is only confirmed when it is published here (<a href="https://dbv.turnier.de/sport/events.aspx?id=8e25686d-193f-4971-a464-fd9d6a9af5cc">link</a>) and is not marked as "substitute". If your registration is not listed there within a week, please contact:</p>
+    <p>The registration is only confirmed when it is published here (<a href="https://dbv.turnier.de/sport/events.aspx?id=1eb702e0-4333-44f8-bbeb-fe5de2e91269">link</a>) and is not marked as "substitute". If your registration is not listed there within a week, please contact:</p>
     <p><a href="mailto:anmeldung@alemannen-cup.de">anmeldung@alemannen-cup.de</a></p>
 
     <p>Here are the details:</p>
